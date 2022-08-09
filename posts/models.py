291e2101b7a8a6e390d5626d6731e2c6124c60f6
relicavaiden <dirrickricks@gmail.com>
2022-08-08 19:30:10 -0500

    Finished Clone

---
+++ posts/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from cloudinary.models import CloudinaryField
+
+class Post(models.Model):
+    class Meta(object):
+        db_table = 'post'
+
+    name = models.CharField(
+        'Name', blank=False, null=False, max_length=14, db_index=True
+    )
+    body = models.CharField(
+        'Body', blank=True, null=True, max_length=140, db_index=True
+    )
+    like = models.IntegerField(
+        'like', default=0, blank= True
+    )
+    image = CloudinaryField(
+        "imgs", blank=True, 
+    )
+    created_at = models.DateTimeField(
+        'Created DateTime', blank=True, auto_now_add=True
+    )
---
+++ static/js/posts.js
@@ -0,0 +1,10 @@
+// JavaScript for Posts Page
+
+$ (function() {
+    // Executed when js-menu-icon js clicked
+    $('.js-menu-icon').click(function(){
+    
+       $(this).next().toggle();
+        
+    })
+})
